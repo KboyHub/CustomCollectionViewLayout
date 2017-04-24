b8ede1f8d8b6fdcfd5d9d640ad8bf9d4c3241665
Kboy <yankang_hpu@163.com>
2017-04-24 16:53:03 +0800

    Initial Commit

---
+++ CustomCollectionViewLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCollectionViewLayout
+//
+//  Created by yankang on 2017/4/24.
+//  Copyright © 2017年 yankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomCollectionViewLayout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomCollectionViewLayout
+//
+//  Created by yankang on 2017/4/24.
+//  Copyright © 2017年 yankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
