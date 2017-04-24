2bb24bd4da82b6e0ae4fd5797bb88913972056e0
Kboy <yankang_hpu@163.com>
2017-04-24 19:04:15 +0800

    修改位置

---
+++ CustomCollectionViewLayout/CustomCell/ColumnCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  ColumnCollectionViewCell.m
+//  CustomCollectionViewLayout
+//
+//  Created by yankang on 2017/4/24.
+//  Copyright © 2017年 yankang. All rights reserved.
+//
+
+#import "ColumnCollectionViewCell.h"
+
+@implementation ColumnCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
---
+++ CustomCollectionViewLayout/CustomCell/RowCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  RowCollectionViewCell.m
+//  CustomCollectionViewLayout
+//
+//  Created by yankang on 2017/4/24.
+//  Copyright © 2017年 yankang. All rights reserved.
+//
+
+#import "RowCollectionViewCell.h"
+
+@implementation RowCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
